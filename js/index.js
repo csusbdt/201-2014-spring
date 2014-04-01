71f98ae3f322bb8c71adee12c8ecc6909d8ed3a2
David Turner <csusbdt@gmail.com>
2014-03-31 18:33:51 -0700

    X

--- js/index.js
+++ js/index.js
@@ -3,16 +3,14 @@
   var jan = 0, feb = 1, mar = 3, apr = 4, may = 5, jun = 6, jul = 7;
 
   schedule = [
-    ['L1',  20, new Date('2014-04-04T23:59:59'), '<a href="L1.html">L1: Command Line</a>'],
-    ['L2',  30, new Date('2014-04-11T23:59:59'), '<a href="L2.html">L2: Expressions</a>'],
-    ['A1',  50, new Date('2014-04-12T23:59:59'), '<a href="A1.pdf">A1: Analyzing Code</a>'],
-    ['Q1', 200, new Date('2014-04-16T23:59:59'), '<a href="Q1.html">Quiz 1</a>'],
-    ['L3',  40, new Date('2014-04-18T23:59:59'), '<a href="L3.html">L3: Algorithms</a>'],
-    ['L4',  30, new Date('2014-04-25T23:59:59'), '<a href="L4.html">L4: Functions</a>'],
-    ['L5',  20, new Date('2014-05-02T23:59:59'), '<a href="L5.html">L5: Classes</a>'],
-    ['A2',  55, new Date('2014-05-03T23:59:59'), '<a href="A2.pdf">A2: Classes</a>'],
+    ['L1',  30, new Date('2014-04-07T23:59:59'), '<a href="L1.html">L1: Command Line</a>'],
+    ['L2',  80, new Date('2014-04-14T23:59:59'), '<a href="L2.html">L2: Expressions</a>'],
+    ['L3',  40, new Date('2014-04-21T23:59:59'), '<a href="L3.html">L3: Algorithms</a>'],
+    ['Q1', 200, new Date('2014-04-23T23:59:59'), '<a href="Q1.html">Quiz 1</a>'],
+    ['L4',  40, new Date('2014-04-28T23:59:59'), '<a href="L4.html">L4: Functions</a>'],
+    ['L5',  60, new Date('2014-05-05T23:59:59'), '<a href="L5.html">L5: Classes</a>'],
     ['Q2', 200, new Date('2014-05-07T23:59:59'), '<a href="Q2.html">Quiz 2</a>'],
-    ['L6',  30, new Date('2014-05-09T23:59:59'), '<a href="L6.html">L6: Arrays</a>'],
+    ['L6',  30, new Date('2014-05-12T23:59:59'), '<a href="L6.html">L6: Arrays</a>'],
     ['L7',  20, new Date('2014-05-16T23:59:59'), '<a href="L7.html">L7: Binary Search</a>'],
     ['A3',  50, new Date('2014-05-17T23:59:59'), '<a href="A3.pdf">A3: Algorithms 2</a>'],
     ['Q3', 200, new Date('2014-05-21T23:59:59'), '<a href="Q3.html">Quiz 3</a>'],
